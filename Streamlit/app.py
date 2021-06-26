efb2be3acb499ff908089fc74b729dbb125e8a8e
Rami Janini <>
2021-06-27 00:35:32 +0300

    Updated app.

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -19,8 +19,6 @@ st.set_page_config(
         layout="wide")
 
 
-
-
 matplotlib.use("agg")
 _lock = RendererAgg.lock
 
@@ -33,7 +31,7 @@ row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.beta_columns(
 
 st.cache(persist=True)
 def load_data():
-    covid19 = pd.read_csv('data/owid-covid-data.csv', encoding='ISO-8859-1',thousands='.', decimal=',', engine='python')
+    covid19 = pd.read_csv('data/covid.csv', encoding='ISO-8859-1',thousands='.', decimal=',', engine='python')
     covid19['date'] = pd.to_datetime(covid19['date'],format = '%Y-%m-%d')
     return covid19
 
@@ -64,7 +62,7 @@ def main():
 
     # TOKYO COMPONENT
     with row3_1, _lock:
-        st.subheader("Tokyo Data 🇯🇵 ")
+        st.header("Tokyo Data 🇯🇵 ")
 
         # Load Tokyo data.
         @st.cache(allow_output_mutation=True)
@@ -75,23 +73,15 @@ def main():
         tsm_df_tokyo = load_data()[1]
         data_load_state.text('')
 
+
         st.dataframe(load_data()[2])
         # year_to_filter = st.slider('Filter years', 2019, 2021, 2021, key='tokyo')  # min: 0h, max: 23h, default: 17h
-        st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
-
-        country = 'Japan'
-        st.header("Japan Covid Cases")
-        fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
-        fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
-        fig.update_yaxes(title_text='New Cases')
-        fig.update_xaxes(title_text='Date')
-        st.plotly_chart(fig)
-
+        st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
 
 
 
     with row3_2, _lock:
-        st.subheader("Plot of Tokyo's Data")
+        st.header("Plot of Tokyo's Data")
         chl_df_tokyo.sort_values(by='Time', ascending=True, inplace=True)
         chl_df_tokyo['time'] = pd.to_datetime(chl_df_tokyo.Time)
         # chl_df_tokyo = chl_df_tokyo[chl_df_tokyo['time'].dt.year == year_to_filter]
@@ -108,29 +98,31 @@ def main():
         st.pyplot(fig)
 
 
-        # fig = Figure()
-        # ax = fig.subplots()
-        # # plt.figure(figsize=(15, 15))
-        #
-        # ax.plot(tsm_df_tokyo['Time'], tsm_df_tokyo['Measurement Value'], color='darkorange',  label='Chl Concentration')
-        # ax.legend()
-        # ax.tick_params(labelsize=7)
-        # ax.set_xlabel('Time (YY/MM)', fontsize=10)
-        # ax.set_ylabel('Measurement value (molarity)', fontsize=10)
-        # st.pyplot(fig)
-
-        # st.markdown("Looks like the average publication date is around **{}**, with your oldest book being **{}** and your youngest being ****.")
-        # st.markdown("Note that the publication date on Goodreads is the **last** publication date, so Chl Concentration is altered for any book that has been republished by a publisher.")
-
 
     st.write('')
-    st.markdown("""---""")
     row4_space1, row4_1, row4_space2, row4_2, row4_space3 = st.beta_columns(
         (.1, 1, .1, 1, .1))
 
-    # VENICE COMPONENT
     with row4_1, _lock:
-        st.subheader("Venice Data 🇮🇹")
+        country = 'Japan'
+        st.header("Japan Covid Cases")
+        fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
+        fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
+        fig.update_yaxes(title_text='New Cases')
+        fig.update_xaxes(title_text='Date')
+        st.plotly_chart(fig)
+
+    with row4_2, _lock:
+        st.header("Cool dates")
+
+    #ITALY
+    st.write('')
+    st.markdown("""---""")
+    row5_space1, row5_1, row5_space2, row5_2, row5_space3 = st.beta_columns(
+        (.1, 1, .1, 1, .1))
+
+    with row5_1, _lock:
+        st.header("Venice Data 🇮🇹")
         @st.cache(allow_output_mutation=True)
         def load_data():
             return veniceData()
@@ -141,24 +133,15 @@ def main():
         st.dataframe(load_data()[2])
         # year_to_filter = st.slider('Filter years', 2019, 2021, 2021, key='venice')  # min: 0h, max: 23h, default: 17h
         st.markdown("Note that the publication date on Goodreads is the **last** publication date, so Chl Concentration is altered for any book that has been republished by a publisher.")
-        country = 'Italy'
-        st.header("Japan Covid Cases")
-        fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
-        fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
-        fig.update_yaxes(title_text='New Cases')
-        fig.update_xaxes(title_text='Date')
-        st.plotly_chart(fig)
 
-    with row4_2, _lock:
-        st.subheader("Plot of Venice's Data ")
+
+    with row5_2, _lock:
+        st.header("Plot of Venice's Data ")
         chl_df_venice.sort_values(by='Time', ascending=True, inplace=True)
         chl_df_venice['time'] = pd.to_datetime(chl_df_venice.Time)
         # chl_df_venice = chl_df_venice[chl_df_tokyo['time'].dt.year == year_to_filter]
-
         fig = Figure()
         ax = fig.subplots()
-        # plt.figure(figsize=(15, 15))
-
         ax.plot(chl_df_venice['Time'], chl_df_venice['Measurement Value'], color='darkorange',  label='Chl Concentration')
         ax.legend()
         ax.tick_params(labelsize=7)
@@ -166,15 +149,30 @@ def main():
         ax.set_ylabel('Measurement value (molarity)', fontsize=10)
         st.pyplot(fig)
 
-
     st.write('')
-    st.markdown("""---""")
-    row5_space1, row5_1, row5_space2, row5_2, row5_space3 = st.beta_columns(
+    row6_space1, row6_1, row6_space2, row6_2, row6_space3 = st.beta_columns(
         (.1, 1, .1, 1, .1))
 
-    # NEW YORK COMPONENT
-    with row5_1, _lock:
-        st.subheader("New York Data 🇺🇸 ")
+    with row6_1, _lock:
+        country = 'Italy'
+        st.header("Italy Covid Cases")
+        fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
+        fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
+        fig.update_yaxes(title_text='New Cases')
+        fig.update_xaxes(title_text='Date')
+        st.plotly_chart(fig)
+
+    with row6_2, _lock:
+        st.header("Cool dates")
+
+    #UNITED STATES
+    st.write('')
+    st.markdown("""---""")
+    row7_space1, row7_1, row7_space2, row7_2, row7_space3 = st.beta_columns(
+        (.1, 1, .1, 1, .1))
+
+    with row7_1, _lock:
+        st.header("New York Data 🇺🇸 ")
         @st.cache(allow_output_mutation=True)
         def load_data():
             return newYorkData()
@@ -185,17 +183,11 @@ def main():
         st.dataframe(load_data()[2])
         # year_to_filter = st.slider('Filter years', 2019, 2021, 2021, key='new-york')  # min: 0h, max: 23h, default: 17h
         st.markdown("Note that the publication date on Goodreads is the **last** publication date, so Chl Concentration is altered for any book that has been republished by a publisher.")
-        country = 'United States'
-        st.header("Japan Covid Cases")
-        fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
-        fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
-        fig.update_yaxes(title_text='New Cases')
-        fig.update_xaxes(title_text='Date')
-        st.plotly_chart(fig)
 
 
-    with row5_2, _lock:
-        st.subheader("Plot of New-York's Data")
+
+    with row7_2, _lock:
+        st.header("Plot of New-York's Data")
         chl_df_newyork.sort_values(by='Time', ascending=True, inplace=True)
         chl_df_newyork['time'] = pd.to_datetime(chl_df_newyork.Time)
         # chl_df_newyork = chl_df_newyork[chl_df_tokyo['time'].dt.year == year_to_filter]
@@ -203,13 +195,28 @@ def main():
         fig = Figure()
         ax = fig.subplots()
         # plt.figure(figsize=(15, 15))
-
         ax.plot(chl_df_newyork['Time'], chl_df_newyork['Measurement Value'], color='darkorange',  label='Chl Concentration')
         ax.legend()
         ax.tick_params(labelsize=7)
         ax.set_xlabel('Time (YY/MM)', fontsize=10)
         ax.set_ylabel('Measurement value (molarity)', fontsize=10)
         st.pyplot(fig)
+
+    st.write('')
+    row8_space1, row8_1, row8_space2, row8_2, row8_space3 = st.beta_columns(
+        (.1, 1, .1, 1, .1))
+
+    with row8_1, _lock:
+        country = 'United States'
+        st.header("United States Covid Cases")
+        fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
+        fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
+        fig.update_yaxes(title_text='New Cases')
+        fig.update_xaxes(title_text='Date')
+        st.plotly_chart(fig)
+
+    with row8_2, _lock:
+        st.header("Cool dates")
 
 if __name__ == '__main__':
     main()
