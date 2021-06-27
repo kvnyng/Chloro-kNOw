86dc8f1c08072798289933ab1978f2f769f56ea0
Rami Janini <>
2021-06-27 12:14:11 +0300

    Nimbus V.1

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -88,7 +88,7 @@ def main():
 
         fig = Figure()
         ax = fig.subplots()
-        # plt.figure(figsize=(15, 15))
+        plt.figure(figsize=(15, 15))
 
         ax.plot(chl_df_tokyo['Time'], chl_df_tokyo['Measurement Value'], color='darkorange',  label='Chl Concentration')
         ax.legend()
@@ -97,23 +97,56 @@ def main():
         ax.set_ylabel('Measurement value (molarity)', fontsize=10)
         st.pyplot(fig)
 
+        # fig=px.bar(chl_df_tokyo,x='Time',y='Measurement Value')
+        # fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
+        # fig.update_yaxes(title_text='Chlorophyll-A Concentration (Molarity)')
+        # fig.update_xaxes(title_text='Date')
+        # st.plotly_chart(fig)
+
+
 
 
     st.write('')
     row4_space1, row4_1, row4_space2, row4_2, row4_space3 = st.beta_columns(
         (.1, 1, .1, 1, .1))
 
+
+    with row4_2, _lock:
+        st.header("Cool dates for Japan")
+        st.markdown('''The concentration of Chlorophyll-a is tested in lakes to determine how much algae is in the lake. Our solution shows the relationship between human activities and algae bloom, through out the COVID-19 pandemic human activities became decreasing and in result the concentration of Chl-a began decreasing to understand the dates below you must understand this first:
+        \n Any decrease of the concentration of CHL was caused because of the high reported COVID-19 cases which meant less human activity in the past two months of the CHL decrease date.
+        \n On the other hand, any increase of the concentration of CHL was caused because of the decrease of the reported COVID-19 cases which meant more human activity in the past two months of the CHL increase date.
+        ''')
+        expander_1 = st.beta_expander("The largest drop of concentration reported at 2020-08-22 in Japan.")
+        expander_1.write('The largest drop of concentration was reported in 2020-08-22 with a decrease value of -87.3 in Tokyo, and if we look at the month before the date 2020-08-22 we can see the increase of COVID cases in Japan during the month of June and July of 2020:')
+        button_1 = expander_1.button('Visualzie', key='3')
+        button_1_end = expander_1.button('Stop Visualizing', key='3')
+
+
+        expander_2 = st.beta_expander("The largest increase of concentration reported at 2019-11-09 in Japan.")
+        expander_2.write('''The largest increase in the CHL concentration was reported in 2019-11-09 with an increase value of +265.03 in Tokyo, and if we take a look at the months before June we can realize that the increase happened before the Pandemic. ''')
+
     with row4_1, _lock:
         country = 'Japan'
         st.header("Japan Covid Cases")
-        fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
-        fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
-        fig.update_yaxes(title_text='New Cases')
-        fig.update_xaxes(title_text='Date')
-        st.plotly_chart(fig)
-
-    with row4_2, _lock:
-        st.header("Cool dates")
+        if button_1:
+            japan_covid_fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases', color='tokyo_increase_color')
+            japan_covid_fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True)
+            japan_covid_fig.update_yaxes(title_text='New Cases')
+            japan_covid_fig.update_xaxes(title_text='Date')
+            st.plotly_chart(japan_covid_fig)
+        elif button_1_end:
+            japan_covid_fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
+            japan_covid_fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True)
+            japan_covid_fig.update_yaxes(title_text='New Cases')
+            japan_covid_fig.update_xaxes(title_text='Date')
+            st.plotly_chart(japan_covid_fig)
+        else:
+            japan_covid_fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
+            japan_covid_fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True)
+            japan_covid_fig.update_yaxes(title_text='New Cases')
+            japan_covid_fig.update_xaxes(title_text='Date')
+            st.plotly_chart(japan_covid_fig)
 
     #ITALY
     st.write('')
@@ -148,22 +181,60 @@ def main():
         ax.set_xlabel('Time (YY/MM)', fontsize=10)
         ax.set_ylabel('Measurement value (molarity)', fontsize=10)
         st.pyplot(fig)
+        # fig=px.bar(chl_df_venice,x='Time',y='Measurement Value')
+        # fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
+        # fig.update_yaxes(title_text='Chlorophyll-A Concentration (Molarity)')
+        # fig.update_xaxes(title_text='Date')
+        # st.plotly_chart(fig)
+
 
     st.write('')
     row6_space1, row6_1, row6_space2, row6_2, row6_space3 = st.beta_columns(
         (.1, 1, .1, 1, .1))
 
-    with row6_1, _lock:
-        country = 'Italy'
-        st.header("Italy Covid Cases")
-        fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
-        fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
-        fig.update_yaxes(title_text='New Cases')
-        fig.update_xaxes(title_text='Date')
-        st.plotly_chart(fig)
 
     with row6_2, _lock:
-        st.header("Cool dates")
+        st.header("Cool dates for Italy")
+        st.markdown('''The concentration of Chlorophyll-a is tested in lakes to determine how much algae is in the lake. Our solution shows the relationship between human activities and algae bloom, through out the COVID-19 pandemic human activities became decreasing and in result the concentration of Chl-a began decreasing to understand the dates below you must understand this first:
+        \n Any decrease of the concentration of CHL was caused because of the high reported COVID-19 cases which meant less human activity in the past two months of the CHL decrease date.
+        \n On the other hand, any increase of the concentration of CHL was caused because of the decrease of the reported COVID-19 cases which meant more human activity in the past two months of the CHL increase date.
+        ''')
+        expander_1 = st.beta_expander("The largest drop of concentration reported at 2020-08-22.")
+        expander_1.write('The largest drop of Chl-a concentration was reported in 2021-03-06 with a decrease value of -79.630008 in Venice, and if we look at the months before the date 2021-03-06  we can see the increase of COVID cases in Italy during the months of January and February of 2021:')
+        button_2 = expander_1.button('Visualzie', key='12')
+        button_2_end = expander_1.button('Stop Visualzing', key='11')
+
+
+
+        expander_2 = st.beta_expander("The largest increase of concentration reported at 2019-11-09.")
+        expander_2.write('''The largest increase in the CHL concentration was reported in 2019-06-13 with an increase value of +400.38177 in Venice, and if we take a look at the months before June we can realize that the increase happened before the Pandemic. ''')
+
+
+    with row6_1, _lock:
+        if button_2:
+            country = 'Italy'
+            st.header("Italy Covid Cases")
+            italy_covid_fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases', color='italy_increase_color')
+            italy_covid_fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True)
+            italy_covid_fig.update_yaxes(title_text='New Cases')
+            italy_covid_fig.update_xaxes(title_text='Date')
+            st.plotly_chart(italy_covid_fig)
+        elif button_2_end:
+            country = 'Italy'
+            st.header("Italy Covid Cases")
+            italy_covid_fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
+            italy_covid_fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True)
+            italy_covid_fig.update_yaxes(title_text='New Cases')
+            italy_covid_fig.update_xaxes(title_text='Date')
+            st.plotly_chart(italy_covid_fig)
+        else:
+            country = 'Italy'
+            st.header("Italy Covid Cases")
+            italy_covid_fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
+            italy_covid_fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True)
+            italy_covid_fig.update_yaxes(title_text='New Cases')
+            italy_covid_fig.update_xaxes(title_text='Date')
+            st.plotly_chart(italy_covid_fig)
 
     #UNITED STATES
     st.write('')
@@ -201,6 +272,14 @@ def main():
         ax.set_xlabel('Time (YY/MM)', fontsize=10)
         ax.set_ylabel('Measurement value (molarity)', fontsize=10)
         st.pyplot(fig)
+        # fig=px.bar(chl_df_newyork,x='Time',y='Measurement Value')
+        # fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
+        # fig.update_yaxes(title_text='Chlorophyll-A Concentration (Molarity)')
+        # fig.update_xaxes(title_text='Date')
+        # st.plotly_chart(fig)
+
+
+
 
     st.write('')
     row8_space1, row8_1, row8_space2, row8_2, row8_space3 = st.beta_columns(
@@ -216,7 +295,8 @@ def main():
         st.plotly_chart(fig)
 
     with row8_2, _lock:
-        st.header("Cool dates")
+        st.header("Cool dates for New York")
+        st.markdown('Coming soon...')
 
 if __name__ == '__main__':
     main()
